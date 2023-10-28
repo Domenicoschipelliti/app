37f8d71b30460cce101db5653d905595fb0077fd
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-10-29 01:32:42 +0200

    Enamorado

--- src/Componets/Main.jsx
+++ src/Componets/Main.jsx
@@ -1,44 +1,64 @@
 import { Component } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 class Ricerca extends Component {
   state = {
     Search: [],
   };
 
-  componentDidMount = async () => {
-    try {
-      const risposta = await fetch(
-        "http://www.omdbapi.com/?apikey=5bd93fb3&s=harry%20potter"
-      );
-
-      if (risposta.ok) {
-        const demanda = await risposta.json();
-        console.log(demanda);
-        this.setState({ Search: demanda });
-      } else {
-        console.log("errore");
-      }
-    } catch (error) {
-      console.log("errore identficato", error);
-    }
+  Movies = () => {
+    fetch("http://www.omdbapi.com/?apikey=5bd93fb3&s=harry%20potter")
+      .then((data) => {
+        if (data.ok) {
+          console.log("dati", data);
+          return data.json();
+        } else {
+          console.log("errore grave nella get");
+        }
+      })
+      .then((datazione) => {
+        console.log("dati eccoli", datazione);
+        this.setState((dato) => ({
+          Search: [
+            ...dato.Search,
+            ...datazione.Search.map((film) => ({
+              Title: film.Title,
+              Year: film.Year,
+              imdbID: film.imdbID,
+              Type: film.Type,
+              Poster: film.Poster,
+            })),
+          ],
+        }));
+      })
+      .catch((err) => {
+        throw new Error("errore in console", err);
+      });
   };
 
+  componentDidMount() {
+    this.Movies();
+  }
   render() {
     <h3 className="text-light ms-4">Anime</h3>;
+
     return (
-      <Container style={{ margin: 220 }}>
+      <Container>
         <Row>
-          <Col md={8} key={this.state.Search.imdbID}>
-            <Card>
-              <Card.Img variant="top" src={this.state.Search.Poster} />
-              <Card.Body>
-                <Card.Title>{this.state.Search.Title}</Card.Title>
-                <Card.Text>{this.state.Search.Type}</Card.Text>
-                <Card.Text>{this.state.Search.Year}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
+          {this.state.Search.map((ricerca) => {
+            return (
+              <Col md={8} key={ricerca.imdbID}>
+                <Card>
+                  <Card.Img variant="top" src={ricerca.Poster} />
+                  <Card.Body>
+                    <Card.Title>{ricerca.Title}</Card.Title>
+                    <Card.Text>{ricerca.Type}</Card.Text>
+                    <Card.Text>{ricerca.Year}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     );
